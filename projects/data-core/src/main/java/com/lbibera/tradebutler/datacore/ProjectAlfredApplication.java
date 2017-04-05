4d1229188f128243a09fa757c2a2312333f21857
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-04-05 17:31:20 +0800

    DEVOPS-0002: generated project from spring initializer

---
+++ projects/data-core/src/main/java/com/lbibera/tradebutler/datacore/ProjectAlfredApplication.java
@@ -0,0 +1,12 @@
+package com.lbibera.tradebutler.datacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAlfredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAlfredApplication.class, args);
+	}
+}
