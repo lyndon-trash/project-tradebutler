94b1255c2ed7341817da33a68bdd07ded228c92a
Lyndon Michael Bibera <hack.of.all.codes@gmail.com>
2017-04-13 16:26:23 +0800

    case changes in the filename

---
+++ projects/data-core/src/test/java/com/lbibera/tradebutler/datacore/corporation/rest/CorporationRestDocumentation.java
@@ -0,0 +1,53 @@
+package com.lbibera.tradebutler.datacore.corporation.rest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorporationRestDocumentation {
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
+    }
+
+    @Transactional
+    @Test @Sql("../repository/CorporationRepositoryTest.findByIdShouldReturnCorrectCorporation.sql")
+    public void getWithCorporationsShouldReturnAValidResponse() throws Exception {
+        this.mockMvc
+                .perform(get("/corporations").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("corporations-list"));
+    }
+
+}
